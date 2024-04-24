a5e14b21f15f7947b177ecb3846be28200c94404
ChidiBrokeDNet <59733962+devevangel@users.noreply.github.com>
2024-04-24 20:25:20 +0100

    started work in mainpage

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
-
+import "./styles.css";
 
 function MainPage() {
-    return (
-        <section>
-            <p>Main page</p>
-        </section>
-    )
+  return (
+    <section className="main-page">
+      <section className="main-page-nav"></section>
+      <section className="columns-list">
+        <div className="column-item"></div>
+        <div className="column-item"></div>
+        <div className="column-item"></div>
+      </section>
+    </section>
+  );
 }
 
 export default MainPage;
